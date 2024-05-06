3b5053d4d62d8e63754a2d5b1f645bf058e02a3d
LarysaK <karahieorhiilarysa@gmail.com>
2024-05-06 13:25:51 +0200

    beforeEach added

--- cypress/e2e/connection/login.cy.js
+++ cypress/e2e/connection/login.cy.js
@@ -1,8 +1,11 @@
 describe('connect page', () => {
-    it('login form', () => {
-      cy.visit('http://localhost:8080/#/login')
-      cy.get('[data-cy="login-input-username]').type('test2@test.fr')
-      cy.get('[data-cy="login-input-password]').type('testtest')
-      cy.get('[data-cy="login-submit"]').click()
-    })
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/#/login');
+  });
+
+  it('login form', () => {
+    cy.get('[data-cy="login-input-username]').type('test2@test.fr')
+    cy.get('[data-cy="login-input-password]').type('testtest')
+    cy.get('[data-cy="login-submit"]').click()
   })
+})
