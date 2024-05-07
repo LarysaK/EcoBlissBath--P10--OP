0cef4a148ba7a9818a11f4a8e7a69b1b5cb4eede
LarysaK <karahieorhiilarysa@gmail.com>
2024-05-07 11:18:11 +0200

    added check for basket after login

--- cypress/e2e/connection/login.cy.js
+++ cypress/e2e/connection/login.cy.js
@@ -7,5 +7,8 @@ describe('connect page', () => {
     cy.get('[data-cy="login-input-username"]').type('test2@test.fr')
     cy.get('[data-cy="login-input-password"]').type('testtest')
     cy.get('[data-cy="login-submit"]').click()
+
+        // After login, assert the presence of the basket link
+        cy.get('[data-cy="nav-link-cart"]').should('be.visible');
   })
 })
